a10e9f9d5a38898c308f4e66d9284d540803be3c
root <root@bill-VirtualBox.(none)>
2013-09-09 22:45:14 +0800

    delete some files

---
+++ projects/head.h
@@ -0,0 +1,13 @@
+#ifndef __FUSE_INFORMATION      /* Headfile start */
+#define __FUSE_INFORMATION
+
+#ifdef __cplusplus      /* extern c declare start */
+extern "c" {
+#endif
+
+
+
+#ifdef __cplusplus      /*extern c declare end */
+#endif /* Headfile end */
+
+
